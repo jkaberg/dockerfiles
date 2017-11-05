c6dfd1910f939a7cbdb8f7aeb3e8c61a435f7910
Joel Kåberg <joel.kaberg@gmail.com>
2017-11-05 11:26:34 +0100

    redirect msgs

--- transmission/run.sh
+++ transmission/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-usermod -o -u "$GID" transmission
-groupmod -o -g "$UID" transmission
+usermod -o -u "$GID" transmission > /dev/null 2>&1
+groupmod -o -g "$UID" transmission > /dev/null 2>&1
 
 chown -R transmission:transmission /downloads /config /watch
 
--- weechat/run.sh
+++ weechat/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-usermod -o -u "$GID" weechat
-groupmod -o -g "$UID" weechat
+usermod -o -u "$GID" weechat > /dev/null 2>&1
+groupmod -o -g "$UID" weechat > /dev/null 2>&1
 
 chown -R weechat:weechat /weechat
 
